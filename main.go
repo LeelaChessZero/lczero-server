e18bbe015d00e3fc7af476de9af5e4726ec94514
borg323 <39573933+borg323@users.noreply.github.com>
2018-09-15 16:29:48 +0300

    Only show the network numbers instead of IDs (#26)
    * Only show the network numbers instead of IDs
    
    * preload BestNetwork

--- main.go
+++ main.go
@@ -772,7 +772,7 @@ func getProgress(trainingRunID uint) ([]gin.H, map[uint]float64, error) {
 				"rating": elo + matchElo,
 				"best":   best,
 				"sprt":   sprt,
-				"id":     network.ID,
+				"id":     network.NetworkNumber,
 			})
 			if !matches[matchIdx].TestOnly && matches[matchIdx].Passed {
 				elo += matchElo
@@ -1061,7 +1061,7 @@ func viewNetworks(c *gin.Context) {
 
 func viewTrainingRuns(c *gin.Context) {
 	training_runs := []db.TrainingRun{}
-	err := db.GetDB().Find(&training_runs).Error
+	err := db.GetDB().Preload("BestNetwork").Find(&training_runs).Error
 	if err != nil {
 		log.Println(err)
 		c.String(500, "Internal error")
@@ -1074,7 +1074,7 @@ func viewTrainingRuns(c *gin.Context) {
 			"id":            training_run.ID,
 			"active":        training_run.Active,
 			"trainParams":   training_run.TrainParameters,
-			"bestNetworkId": training_run.BestNetworkID,
+			"bestNetwork":   training_run.BestNetwork.NetworkNumber,
 			"description":   training_run.Description,
 		})
 	}
